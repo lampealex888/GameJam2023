c4175c1124ec3e7f60a5155a6a6964d6ea943335
lampealex888 <lampealex888@gmail.com>
2023-10-07 14:48:50 -0400

    added main menu

---
+++ Assets/_Scripts/Menu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Add me!!
+
+public class Menu : MonoBehaviour
+{
+    public void OnPlayButton ()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void OnCreditsButton()
+    {
+        SceneManager.LoadScene(8);
+    }
+}
